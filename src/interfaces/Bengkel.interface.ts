08b6e4f4531710e38646d542040fed428299f3d6
FortinoHogan <fortinohogan@gmail.com>
2026-04-19 18:47:27 +0700

    feat: add bengkel page detail

--- src/interfaces/Bengkel.interface.ts
+++ src/interfaces/Bengkel.interface.ts
@@ -16,6 +16,7 @@ export interface IBengkelModule extends IModel {
   environmentId: number
   baseUrl: string
   basicAuth: string
+  postmanEnv: JSON
 }
 
 export interface IBengkelDetail extends IModel {
